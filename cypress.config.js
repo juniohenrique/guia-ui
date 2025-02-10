e21f8db3d6d38801c815fc79a9f5ea831d566048
“Junio <juunio.henriquee@gmail.com>
2025-02-10 10:59:45 -0300

    add github actions to project

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-     baseUrl: 'https://automationexercise.com',
+    baseUrl: 'https://automationexercise.com',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    }
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  }
 });
